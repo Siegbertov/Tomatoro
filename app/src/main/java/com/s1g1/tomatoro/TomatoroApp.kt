130e2cf795d0a4a62ae9a749745d2ec2d466adeb
Sieg Bertov <siegbertov@gmail.com>
2026-04-09 19:57:33 +0300

    feat: implement TomatoroApp with basic Koin initialization

---
+++ app/src/main/java/com/s1g1/tomatoro/TomatoroApp.kt
@@ -0,0 +1,25 @@
+package com.s1g1.tomatoro
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class TomatoroApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            // DI EXCEPTIONS
+            androidLogger()
+
+            // PASS APP CONTEXT
+            androidContext(this@TomatoroApp)
+
+            // FUTURE MODULES
+            modules(emptyList())
+        }
+    }
+
+}
