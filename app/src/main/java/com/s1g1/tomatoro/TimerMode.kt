57e1d4d38c13090d1251d7ba80952e3d6a47fcd9
Sieg Bertov <siegbertov@gmail.com>
2026-04-13 16:55:40 +0300

    ui: implement PieChart for stats

--- app/src/main/java/com/s1g1/tomatoro/TimerMode.kt
+++ app/src/main/java/com/s1g1/tomatoro/TimerMode.kt
@@ -1,14 +1,28 @@
 package com.s1g1.tomatoro
 
 import androidx.annotation.StringRes
+import androidx.compose.ui.graphics.Color
 
 enum class TimerMode(
     @StringRes val title: Int,
-    val defaultDuration: Int
+    val color: Color,
+    val defaultDuration: Int,
 ){
-    TOMATORO(title = R.string.tomatoro_session, defaultDuration = 25),
-    BREAK(title = R.string.short_break_session, defaultDuration = 5),
-    LONG_BREAK(title = R.string.long_break_session, defaultDuration = 15);
+    TOMATORO(
+        title = R.string.tomatoro_session,
+        color = Color(0xFFE57373),
+        defaultDuration = 25,
+    ),
+    BREAK(
+        title = R.string.short_break_session,
+        color = Color(0xFF81C784),
+        defaultDuration = 5,
+        ),
+    LONG_BREAK(
+        title = R.string.long_break_session,
+        color = Color(0xFF64B5F6),
+        defaultDuration = 15,
+    );
 
     companion object {
         fun fromName(name: String) : TimerMode{
--- app/src/main/java/com/s1g1/tomatoro/ui/stats/StatsScreen.kt
+++ app/src/main/java/com/s1g1/tomatoro/ui/stats/StatsScreen.kt
@@ -1,18 +1,19 @@
 package com.s1g1.tomatoro.ui.stats
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowLeft
-import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -22,7 +23,13 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.s1g1.tomatoro.TimerMode
 import com.s1g1.tomatoro.database.Session
@@ -32,9 +39,7 @@ fun StatsScreen(
     navController: NavHostController,
     statsViewModel: StatsViewModel
 ) {
-
     val scrollState = rememberScrollState()
-    val allSessions by statsViewModel.allSessions.collectAsState()
 
     val dayLabel by statsViewModel.dayLabel.collectAsState()
     val daySessions by statsViewModel.daySessions.collectAsState()
@@ -52,7 +57,7 @@ fun StatsScreen(
         modifier = Modifier
             .fillMaxSize()
             .verticalScroll(scrollState)
-            .padding(bottom=80.dp),
+            .padding(bottom = 80.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ){
@@ -93,6 +98,19 @@ fun StatsRow(
     onLeftClick: ()->Unit,
     onRightClick: ()->Unit,
 ){
+
+    val totalTime = sessions.sumOf { it.duration }
+
+    val tomatoroPercentage = if (totalTime!=0L) {
+        sessions.sumOf { if(it.mode== TimerMode.TOMATORO) it.duration else 0 } * 100 / totalTime
+    } else 0
+    val breakPercentage = if (totalTime!=0L) {
+        sessions.sumOf { if(it.mode== TimerMode.BREAK) it.duration else 0 } * 100 / totalTime
+    } else 0
+    val longBreakPercentage = if (totalTime!=0L) {
+        sessions.sumOf { if(it.mode== TimerMode.LONG_BREAK) it.duration else 0 } * 100 / totalTime
+    } else 0
+
     Column(modifier = Modifier.padding(16.dp)) {
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -100,37 +118,89 @@ fun StatsRow(
             verticalAlignment = Alignment.CenterVertically
         ) {
             IconButton(onClick = onLeftClick) {
-                Icon(Icons.Default.KeyboardArrowLeft, contentDescription = null)
+                Icon(Icons.AutoMirrored.Filled.KeyboardArrowLeft, contentDescription = null)
             }
             Text(text = periodLabel)
             IconButton(onClick = onRightClick) {
-                Icon(Icons.Default.KeyboardArrowRight, contentDescription = null)
+                Icon(Icons.AutoMirrored.Filled.KeyboardArrowRight, contentDescription = null)
             }
         }
 
         Card(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(100.dp),
+                .fillMaxWidth(),
             shape = RoundedCornerShape(8.dp),
         ) {
-            val totalTime = sessions.sumOf { it.duration }
-            val totalTomatoro = sessions.sumOf { if(it.mode== TimerMode.TOMATORO) it.duration else 0 }
-            val totalBreak = sessions.sumOf { if(it.mode== TimerMode.BREAK) it.duration else 0 }
-            val totalLongBreak = sessions.sumOf { if(it.mode== TimerMode.LONG_BREAK) it.duration else 0 }
-
-            Column(
+            Row(
                 modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
             ){
-                Text(text = "TOTAL: ${(totalTime/60).toInt()} mins")
-                Text(text = "TOTAL TOMATORO: ${(totalTomatoro/60).toInt()} mins")
-                Text(text = "TOTAL BREAK: ${(totalBreak/60).toInt()} mins")
-                Text(text = "TOTAL LONG BREAK: ${(totalLongBreak/60).toInt()} mins")
+                Canvas(
+                    modifier = Modifier
+                        .size(200.dp)
+                        .padding(10.dp)
+                ){
+                    // OUTLINE
+                    drawCircle(
+                        color = Color.Black.copy(alpha = 0.25f),
+                        radius = size.minDimension / 2,
+                        style = Stroke(width = 4.dp.toPx())
+                    )
 
+                    if (totalTime == 0L){
+
+                        // EMPTY CIRCLE
+                        drawCircle(
+                            color = Color.White,
+                            radius = size.minDimension / 2,
+                        )
+                    } else {
+                        val breakAngle = breakPercentage * 3.6f
+                        val longBreakAngle = longBreakPercentage * 3.6f
+                        val tomatoroAngle = 360f - breakAngle - longBreakAngle
+                        var currentStartAngle = -90f
+
+                        listOf(
+                            TimerMode.BREAK.color to breakAngle,
+                            TimerMode.LONG_BREAK.color to longBreakAngle,
+                            TimerMode.TOMATORO.color to tomatoroAngle
+                        ).forEach { (color, angle) ->
+                            drawArc(
+                                color = color,
+                                startAngle = currentStartAngle,
+                                sweepAngle = angle,
+                                useCenter = true
+                            )
+                            currentStartAngle += angle
+                        }
+                    }
+                }
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalAlignment = Alignment.Start,
+                    verticalArrangement = Arrangement.Center
+                ){
+                    Text(
+                        text = "TOTAL: ${(totalTime/60).toInt()} mins",
+                        fontWeight = FontWeight.Black
+                    )
+                    if(totalTime!=0L){
+                        listOf(
+                            tomatoroPercentage to TimerMode.TOMATORO,
+                            breakPercentage to TimerMode.BREAK,
+                            longBreakPercentage to TimerMode.LONG_BREAK
+                        ).forEach { (percentage, mode) ->
+                            Text(
+                                text = "${stringResource(mode.title)}: $percentage%",
+                                color = mode.color,
+                                fontStyle = FontStyle.Italic,
+                                fontSize = 15.sp
+                            )
+                        }
+                    }
+                }
             }
         }
     }
-
 }
